2f616911740f322c110c72e7ac3f72a405501f80
StudentSYN40 <125072826+StudentSYN40@users.noreply.github.com>
2023-02-21 11:30:06 +0300

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int n = 0;
+    printf("Введите количество элементов :");
+    scanf("%i",&n);
+ 
+    int *arr = new int[n];
+ 
+    for (int i = 0; i < n; i++)
+    {
+    	printf("Введите элемент [ %i ] = ",i+1);
+        scanf("%i",&arr[i]);
+    }
+ 
+    int min = arr[0];
+    int max = arr[0];
+    int sum = 0;
+ 
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] < min)
+        {
+            min = arr[i];
+        }
+        if (arr[i] > max)
+        {
+            max = arr[i];
+        }
+        sum += arr[i];
+    }
+ 
+ 	printf("Максимальное число = %i\n",max);
+ 	printf("Минимальное число = %i\n",min);
+ 	printf("Сумма всех элементов = %i\n",sum);
+ 	printf("Середнее арифметическое элементов = %i\n",sum/n);
+ 
+    delete[] arr;
+}
